4ec2ce816c291a86641fa5aa1ff9c7373e4e3058
Kalin Hristov <kalin0169@gmail.com>
2023-07-19 15:52:52 +0300

    Add mock Tasks for my App

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,37 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    name: 'Bug_1',
+    description: 'Bug when we start the module',
+    hoursOfWork: null,
+    takenByUser: null,
+    inProgress: false,
+    isFinished: false,
+    _createdOn: '01.01.2022',
+    _id: '1',
+    _ownerId: 'Kalin'
+  },
+  {
+    name: 'Bug_2',
+    description: 'Bug with the onboarding emails',
+    hoursOfWork: '2',
+    takenByUser: 'Ivancho',
+    inProgress: false,
+    isFinished: true,
+    _createdOn: '02.01.2022',
+    _id: '2',
+    _ownerId: 'Kalin'
+  },
+  {
+    name: 'Bug_3',
+    description: 'Bug with the Contact Us Page',
+    hoursOfWork: null,
+    takenByUser: null,
+    inProgress: false,
+    isFinished: false,
+    _createdOn: '03.01.2022',
+    _id: '3',
+    _ownerId: 'Ivancho'
+  },
+]
