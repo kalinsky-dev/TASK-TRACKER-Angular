2599fa7cdecb49600d6b33c014137a33d48c38d1
Kalin Hristov <kalin0169@gmail.com>
2023-07-25 11:52:46 +0300

    Update Routing Modules with the correct Paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TaskComponent } from './components/task/task.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
+import { TaskDeleteComponent } from './components/task-delete/task-delete.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'tasks', component: TaskComponent
+  },
+  {
+    path: 'create-task', component: AddTaskComponent
+  },
+  {
+    path: 'tasks/:taskId', component: TaskDetailsComponent
+  },
+  {
+    path: 'tasks/:taskId/delete', component: TaskDeleteComponent
+  },
+  // {
+  //   path: 'logout', component: LogoutComponent
+  // },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: '**', component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
