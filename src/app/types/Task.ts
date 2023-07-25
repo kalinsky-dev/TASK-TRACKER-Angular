6961c4adff8765e5f0bdc90286876bb9ded4ab92
Kalin Hristov <kalin0169@gmail.com>
2023-07-25 14:38:38 +0300

    Update Task.ts

--- src/app/types/Task.ts
+++ src/app/types/Task.ts
@@ -1,11 +1,12 @@
 export interface Task {
   name: string;
   description: string;
-  hoursOfWork: string | null;
-  takenByUser: string | null;
+  owner: string;
   inProgress: boolean;
+  takenByUser: string | false;
+  hoursOfWork: number | false;
   isFinished: boolean;
-  _createdOn: string;
-  _id: string;
-  _ownerId: string;
+  // _createdOn: string;
+  // _id: string;
+  // _ownerId: string;
 }
