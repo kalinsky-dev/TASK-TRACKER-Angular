108e82e9d6d43ed3e7c550e2845f0e72745b6425
Kalin Hristov <kalin0169@gmail.com>
2023-07-27 10:09:09 +0300

    Update the Base Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000', // domain -> will not be localhost in prod
+  baseUrl: 'http://localhost:3000', // domain -> will not be localhost in prod
 };
