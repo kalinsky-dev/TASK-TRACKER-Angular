c109da6a690d3380c8d3b009c3ac98b5f8c7b828
Kalin Hristov <kalin0169@gmail.com>
2023-07-27 09:48:52 +0300

    Create Task Service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getAll() { }
+
+  getOne() { }
+
+  create() { }
+
+  edit() { }
+
+  remove() { }
+  
+}
