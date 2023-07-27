216d715c02ad031fb5576a3085b9b2d347385d1a
Kalin Hristov <kalin0169@gmail.com>
2023-07-27 16:35:53 +0300

    Update User Service to cope with Server Requests!

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/User';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class UserService {
   user: User | undefined;
   USER_KEY = '[user]';
 
-  constructor() {
+  constructor(private http: HttpClient) {
     try {
       const lsUser = localStorage.getItem(this.USER_KEY) || '';
       this.user = JSON.parse(lsUser);
@@ -23,17 +25,43 @@ export class UserService {
     return !!this.user;
   }
 
-  login(): void {
-    this.user = {
-      email: 'kalin@gmail.com',
-      password: '12345'
-    }
+  login(email: string, password: string) {
 
-    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    // this.user = {
+    //   email: 'kalin@gmail.com',
+    //   password: '12345'
+    // }
+    // localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    //ToDo: for now I do not handle the data from the server!
+
+    return this.http
+      .post<User>('/api/users/login', { email, password })
+      .pipe(tap((user) => {
+        this.user = user;
+        localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+      }));
   }
 
-  logout(): void {
-    this.user = undefined;
-    localStorage.removeItem(this.USER_KEY);
+  register(email: string, password: string) {
+    return this.http
+      .post<User>('/api/users/register', { email, password })
+      .pipe(tap((user) => {
+        this.user = user;
+        localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+      }));
+  }
+
+
+  logout() {
+    // this.user = undefined;
+    // localStorage.removeItem(this.USER_KEY);
+    //ToDo: for now I do not work with the server!
+
+    return this.http
+      .post<User>('/api/users/logout', {})
+      .pipe(tap(() => {
+        this.user = undefined;
+        localStorage.removeItem(this.USER_KEY);
+      }));
   }
 }
