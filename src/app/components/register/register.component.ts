0f4766ae3f7e40636a2411172ebfc5b85a404f6d
Kalin Hristov <kalin0169@gmail.com>
2023-07-27 16:33:49 +0300

    Register Works with the Server!

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +13,27 @@ export class RegisterComponent {
 
   @ViewChild('registerForm') registerForm: NgForm | undefined;
 
+  constructor(private userService: UserService, private router: Router) { }
+
   registerSubmitHandler(): void {
     if (!this.registerForm) return;
 
     const form = this.registerForm;
-
+   
     if (form.invalid) {
       return
     }
 
-    const value: { email: string; password: string } = form.value;
+    // const value: { email: string; password: string } = form.value;
     // console.log(value);
-    form.setValue({ email: '', password: '', rePass: '' })
+    // form.setValue({ email: '', password: '', rePass: '' })
+    //ToDo: for now I do not handle the data from the server!
+
+    const { email, password, rePass } = form.value;
+
+    this.userService.register(email, password).subscribe(() => {
+      form.setValue({ email: '', password: '', rePass: '' })
+      this.router.navigate(['/tasks']);
+    });
   }
 }
