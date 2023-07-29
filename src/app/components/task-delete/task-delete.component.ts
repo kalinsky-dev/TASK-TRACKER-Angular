38228cb6e656a0b205a21e659fbde1301c9265dd
Kalin Hristov <kalin0169@gmail.com>
2023-07-29 15:52:35 +0300

    Task-Delete Works!
    Task-Delete Component deletes Data form the Server and navigates to ' /tasks '!

--- src/app/components/task-delete/task-delete.component.ts
+++ src/app/components/task-delete/task-delete.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-delete',
@@ -8,17 +9,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class TaskDeleteComponent implements OnInit {
-  
+
   idToDel!: string;
 
-  constructor(private activeRoute: ActivatedRoute, private router: Router) { }
+  constructor(private activeRoute: ActivatedRoute, private router: Router, private taskService: TaskService) { }
 
   ngOnInit(): void {
     this.idToDel = this.activeRoute.snapshot.params['taskId']
   }
 
   onDeleteHandler() {
-    console.log(this.idToDel);
-    this.router.navigate([`/`])
+    // console.log(this.idToDel);
+    // this.router.navigate([`/`])
+    // ToDo: for now I do not handle the data from the server!
+
+    this.taskService.remove(this.idToDel).subscribe(() => {
+      this.router.navigate(['/tasks']);
+    })
   }
 }
