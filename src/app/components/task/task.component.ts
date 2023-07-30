4d7ea2739db1af6b65e7411c673fa85de15f9dac
Kalin Hristov <kalin0169@gmail.com>
2023-07-30 12:07:05 +0300

    Task Component Render X button!
    Task Component render X Delete Button conditionally according to if the user is Owner or not!

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/types/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-task',
@@ -9,19 +10,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./task.component.css']
 })
 
-export class TaskComponent {
+export class TaskComponent implements OnInit {
 
   @Input() task!: Task;
+  @Input() user!: User;
+
   faTimes = faTimes;
+  ifOwner!: boolean | undefined;
 
   constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.ifOwner = this.user.email == this.task.owner;
+  }
 
   onTaskClickHandler(taskId: string) {
     // console.log(taskId);
     this.router.navigate([`/tasks/${taskId}`])
   }
 
-  onDeleteClickHandler( taskId: string) {
+  onDeleteClickHandler(taskId: string) {
     // console.log(taskId);
     this.router.navigate([`/tasks/${taskId}/delete`])
   }
