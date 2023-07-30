dadde2dbf60c89f9d97869f4649c125521737fe0
Kalin Hristov <kalin0169@gmail.com>
2023-07-30 11:00:49 +0300

    Not Found Component navigates the User!
    Not Found Component navigates the User conditionally according whether he is logged or not!

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-not-found',
@@ -6,6 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+
+  user!: User;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.user = this.userService.user!;
+  }
 
 }
