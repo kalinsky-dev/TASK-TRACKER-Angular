68e55aaa19ac4301316bc285db2a7333dded0830
Kalin Hristov <kalin0169@gmail.com>
2023-07-30 10:56:06 +0300

    About Component navigates correctly the User!
    About Component navigate the User conditionally rather he is logged or not logged in!

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  user!: User;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.user = this.userService.user!;
+  }
 
 }
