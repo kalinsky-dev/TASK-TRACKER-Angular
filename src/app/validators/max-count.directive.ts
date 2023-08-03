9bcc099b7830f84f38e5a65a910790c43ddda361
Kalin Hristov <kalin0169@gmail.com>
2023-08-03 15:30:29 +0300

    Create Max Count Directive.
    Create Max Count Directive to check if the password is longer then 10 Symbols!

---
+++ src/app/validators/max-count.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+  selector: '[appMaxCount]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaxCountDirective,
+      multi: true,
+    }
+  ]
+})
+export class MaxCountDirective implements Validator {
+
+  @Input() appMaxCount: number | undefined;
+
+  constructor() { }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    // debugger;
+    if (this.appMaxCount === undefined || (control.value?.length || 0) <= this.appMaxCount) {
+      return null;
+    }
+
+    return { appMaxCount: this.appMaxCount }
+  }
+}
