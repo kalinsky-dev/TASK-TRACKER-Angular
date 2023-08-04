4db627c0ca54a7ce3259cbd2662d301295e82802
Kalin Hristov <kalin0169@gmail.com>
2023-08-04 14:42:27 +0300

    Hours Validator Works!
    Hour Validator checks for the correct input!

--- src/app/validators/hours-validator.ts
+++ src/app/validators/hours-validator.ts
@@ -2,7 +2,9 @@ import { ValidatorFn } from "@angular/forms";
 
 export function hoursValidator(): ValidatorFn {
   return (control) => {
-    return (!isNaN(control.value) && control.value > 0)
+    return (control.value == '' ||
+      (!isNaN(control.value) &&
+        control.value > 0))
       ? null
       : { hoursValidator: true };
   };
